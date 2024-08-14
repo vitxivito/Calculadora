93e0c2ff5c91d172ce336178365308bac7c8b3b2
Vitoria Garcia <vitoria_garcia_costa_m-tsapp1ma2223@cev.com>
2024-08-14 18:21:07 +0200

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by Vitoria Garcia on 14/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculadora
+//
+//  Created by Vitoria Garcia on 14/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
